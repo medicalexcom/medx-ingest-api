b9bb34dd911b5177285d7fea65fc5c5fc4705f7e
medicalexcom <medicalex.com@gmail.com>
2025-09-16 13:02:04 -0700

    Update index.js to include Shopify and BigCommerce harvesters

--- harvesters/index.js
+++ harvesters/index.js
@@ -1,5 +1,7 @@
 export { extractWordPressTabs } from './wordpress.js';
 export { extractSalesforceTabs } from './salesforce.js';
+export { extractShopifyTabs } from './shopify.js';
+export { extractBigCommerceTabs } from './bigcommerce.js';
 
 export function getTabHarvester(siteType) {
   switch (siteType) {
@@ -7,6 +9,10 @@ export function getTabHarvester(siteType) {
       return extractWordPressTabs;
     case 'salesforce':
       return extractSalesforceTabs;
+    case 'shopify':
+      return extractShopifyTabs;
+    case 'bigcommerce':
+      return extractBigCommerceTabs;
     default:
       return null;
   }
