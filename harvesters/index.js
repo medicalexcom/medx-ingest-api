c8c1f0159dd42b168300adef6b5c26063f806513
medicalexcom <medicalex.com@gmail.com>
2025-09-16 18:45:30 -0700

    Update index.js to include Magento, Squarespace and Wix harvesters

--- harvesters/index.js
+++ harvesters/index.js
@@ -2,6 +2,9 @@ export { extractWordPressTabs } from './wordpress.js';
 export { extractSalesforceTabs } from './salesforce.js';
 export { extractShopifyTabs } from './shopify.js';
 export { extractBigCommerceTabs } from './bigcommerce.js';
+export { extractMagentoTabs } from './magento.js';
+export { extractSquarespaceTabs } from './squarespace.js';
+export { extractWixTabs } from './wix.js';
 
 export function getTabHarvester(siteType) {
   switch (siteType) {
@@ -13,6 +16,12 @@ export function getTabHarvester(siteType) {
       return extractShopifyTabs;
     case 'bigcommerce':
       return extractBigCommerceTabs;
+    case 'magento':
+      return extractMagentoTabs;
+    case 'squarespace':
+      return extractSquarespaceTabs;
+    case 'wix':
+      return extractWixTabs;
     default:
       return null;
   }
